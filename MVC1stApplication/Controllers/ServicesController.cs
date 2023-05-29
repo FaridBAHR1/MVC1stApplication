8a2d02b978ca0d6ff6c695b8a359046e983e92d6
FaridBAHR1 <f4ridb4hri@gmail.com>
2023-05-30 10:55:57 +1100

    Initial commit

---
+++ MVC1stApplication/Controllers/ServicesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC1stApplication.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
